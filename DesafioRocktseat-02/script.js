ffd6560ef0dd6398be0d1621ee466f3b515855b1
Sebastiao Vitor <sebastiaovitor18@gmail.com>
2023-05-09 22:50:57 -0300

    b

--- DesafioRocktseat-02/script.js
+++ DesafioRocktseat-02/script.js
@@ -5,40 +5,40 @@ const students = [
     name: "Amanda Medeiros",
     gradeOne: 8,
     gradeTwo: 7,
-    gradethree: 10
+    gradeThree: 10
   },
   {
     name: "Sebastião Vitor",
     gradeOne: 5,
     gradeTwo: 8.5,
-    gradethree: 8.3
+    gradeThree: 8.3
   },
   {
     name: "Eduardo Santos",
     gradeOne: 10,
     gradeTwo: 9.4,
-    gradethree: 9.8
+    gradeThree: 9.8
   },
   {
     name: "Mateus Santana",
     gradeOne: 0,
     gradeTwo: 1,
-    gradethree: 10
+    gradeThree: 10
   },
   {
     name: "Bianca Almeida",
     gradeOne: 8,
     gradeTwo: 0,
-    gradethree: 9.2
+    gradeThree: 9.2
   },
 ]
 
-let average = function(gradeOne, gradeTwo, gradethree) {
-  return ((gradeOne + gradeTwo + gradethree) / 3).toFixed(1)
+let average = function(gradeOne, gradeTwo, gradeThree) {
+  return ((gradeOne + gradeTwo + gradeThree) / 3).toFixed(1)
 }
 
 for(let student of students) {
-  let resultGrade = average(student.gradeOne, student.gradeTwo, student.gradethree)
+  let resultGrade = average(student.gradeOne, student.gradeTwo, student.gradeThree)
   if(resultGrade >= 7) {
     alert(`
     A média do(a) aluno(a) ${student.name} é: ${resultGrade}
